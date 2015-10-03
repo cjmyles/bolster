658d45e2807e660501cf0a0a0a0c472f45f3059b
Craig Myles <cr@igmyles.com>
2015-10-03 14:11:53 +1000

    Added query string parses

--- libs/url.js
+++ libs/url.js
@@ -10,6 +10,14 @@ module.exports = {
 
   back: function() {
     Backbone.history.history.back();
-  }
+  },
+
+  // http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+  getParameterByName: function(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+      return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  } 
 
 };
